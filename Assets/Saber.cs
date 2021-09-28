280269bfdb09ace5f188372008068a79cc190782
mwoo34 <enmw34@gmail.com>
2021-09-28 16:16:23 +0900

    add first chapter3

---
+++ Assets/Saber.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Saber : MonoBehaviour
+{
+    public LayerMask layer;
+    private Vector3 previousPos;
+    public Canvas[] images;
+    
+    void Start()
+    {
+        //images = GetComponent<Canvas>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 1, layer))
+        {
+            if (Vector3.Angle(transform.position - previousPos, hit.transform.up) > 130)
+            {
+                Destroy(hit.transform.gameObject);
+            }
+            else
+            {
+                DestroyImg();
+            }
+        }
+        previousPos = transform.position;
+    }
+
+    void DestroyImg()
+    {
+        //images.
+    }
+}
